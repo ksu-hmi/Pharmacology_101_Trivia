d91cc30ba81c09409633186ef8e0682a2d53099f
kencraig21 <69943260+kencraig21@users.noreply.github.com>
2020-12-02 15:45:16 -0500

    Added 1 question to test

---
+++ respiratory.py
@@ -0,0 +1,12 @@
+## respiratory 
+
+question1 = ("What medication is used to prevent asthama attacks in patients with bronchial asthma?\n")
+question2 = 
+question3 =
+
+
+
+
+answer1 = ("Cromolyn Sodium(intal)\n")
+answer2 = 
+answer3 = 
