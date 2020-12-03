51e558ca24dcaac2e2f96cc123f6ffa9ec37e7a2
kencraig21 <69943260+kencraig21@users.noreply.github.com>
2020-12-02 22:23:29 -0500

    Added Details to Respiratory

--- respiratory.py
+++ respiratory.py
@@ -1,12 +1,12 @@
 ## respiratory 
 
 question1 = ("What medication is used to prevent asthama attacks in patients with bronchial asthma?\n")
-question2 = 
+question2 = ("True or Fasle: Tiotropium (spiriva) and Ipatropium ( Artovent) are not indicated for acute asthma attacks?\n")
 question3 =
 
 
 
 
 answer1 = ("Cromolyn Sodium(intal)\n")
-answer2 = 
+answer2 = ("True\n")
 answer3 = 
