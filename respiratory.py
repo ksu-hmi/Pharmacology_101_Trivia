4ec5d65844bf2e57c5694eb9768d3be837013bca
kencraig21 <69943260+kencraig21@users.noreply.github.com>
2020-12-04 15:43:09 -0500

    Added 8 questions to Respiratory

--- respiratory.py
+++ respiratory.py
@@ -1,12 +1,19 @@
-## respiratory 
+## respiratory drugs
 
 question1 = ("What medication is used to prevent asthama attacks in patients with bronchial asthma?\n")
 question2 = ("True or Fasle: Tiotropium (spiriva) and Ipatropium ( Artovent) are not indicated for acute asthma attacks?\n")
-question3 =
-
-
-
+question3 = ("Yes or No: Is Antihistamine a type of Bronchodilator\n")
+question4 = ("Which of the following drugs block Acetylcholine in order to prevent bronchoconstriction?\n")
+question5 = ("What drug is a respiratory stimulant\n")
+question6 = ("What drug supresses the cough reflex in the brain\n")
+question7 = ("What is important to note when using Naloxone as a respiratory stimulant?\n")
+question8 = ("Which of the following drugs stimulates Beta-2 receptors causing bronchodilation?\n")
 
 answer1 = ("Cromolyn Sodium(intal)\n")
 answer2 = ("True\n")
-answer3 = 
+answer3 = ("Yes\n")
+answer4 = ("Aminopentamide\n")
+answer5 = ("Doxapram\n")
+answer6 = ("Trimeprazine\n")
+answer7 = ("Reverse Analgesia\n")
+answer8 = ("Albuterol\n")
