35d63a3f41d1f7c4116ce820a4c23e8448dc00f0
kencraig21 <69943260+kencraig21@users.noreply.github.com>
2020-12-07 23:59:38 -0500

    Added details to Neuro

---
+++ Neuro.py
@@ -0,0 +1,11 @@
+## Neurological drugs
+
+question1 = ("What is a common side effect of Carbidopa-levodopa (Sinemet)?\n")
+question2 = (" _______ is used to treat seizures and nerve pain such as trigeminal neuralgia and diabetic neuropathy?\n")
+question3 = ("What is the therapeutic serum range of phenobarbital?\n")
+question4 = ("What s a side effect of Codeine sulfate?\n")
+
+answer1 = ("Increased blood pressure\n")
+answer2 = ("Carbamazepine\n")
+answer3 = ("15-40mg/ml\n")
+answer4 = ("Constipation\n")
